17cbc0ea1f7d69e4c481b805cda7eb910730a1f2
Andy <a.n.kryvoruka@gmail.com>
2021-03-03 21:04:59 +0100

    added addition steps

--- HW_2/google_test.js
+++ HW_2/google_test.js
@@ -1,4 +1,5 @@
-const assert = require('assert')
+const assert = require('assert');
+const { debug } = require('console');
 const webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
@@ -45,11 +46,12 @@ async function attribute () {
   // authorization
   await authorization(driver);
 
-  await driver.manage().setTimeouts({ implicit: 1000 });
+  await driver.manage().setTimeouts({ implicit: 3000 });
   await driver.findElement(By.css(".app:nth-child(2) .name")).click();
-  await driver.manage().setTimeouts({ implicit: 1000 });
-  await driver.findElement(By.css(".doc:nth-child(2) a")).click();
-  await driver.wait(until.elementLocated(By.css("div.panel-heading")), 2000);
+  await driver.manage().setTimeouts({ implicit: 3000 });
+  await driver.wait(until.elementLocated(By.css(".doc:nth-child(2) .name")), 3000);
+  await driver.findElement(By.css(".doc:nth-child(2) .name")).click()
+  await driver.wait(until.elementLocated(By.css("div.panel-heading")), 3000);
   let attribute = await driver.findElement(By.css("div.panel-heading")).getText(); 
   // verify
   await assert.equal(attribute, "Attribute Groups", "Element doesn't match"); 
@@ -57,6 +59,55 @@ async function attribute () {
   await driver.quit();
 }
 
-catalog();
 
+async function manufacturers () {
+  const driver = await new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
+
+  // url
+  await driver.get('http://158.101.173.161/admin/');
+
+  // authorization
+  await authorization(driver);
+
+  await driver.manage().setTimeouts({ implicit: 1000 });
+  await driver.findElement(By.css(".app:nth-child(2) .name")).click();
+  await driver.manage().setTimeouts({ implicit: 1000 });
+  await driver.findElement(By.css(".doc:nth-child(3) .name")).click();
+  await driver.wait(until.elementLocated(By.css("div.panel-heading")), 2000);
+  let manufacturers = await driver.findElement(By.css("div.panel-heading")).getText(); 
+  // verify
+  await assert.equal(manufacturers, "Manufacturers", "Element doesn't match"); 
+  
+  await driver.quit();
+}
+
+async function suppliers () {
+  const driver = await new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
+
+  // url
+  await driver.get('http://158.101.173.161/admin/');
+
+  // authorization
+  await authorization(driver);
+
+  await driver.manage().setTimeouts({ implicit: 1000 });
+  await driver.findElement(By.css(".app:nth-child(2) .name")).click();
+  await driver.manage().setTimeouts({ implicit: 1000 });
+  await driver.findElement(By.css(".doc:nth-child(4) .name")).click();
+  await driver.wait(until.elementLocated(By.css("div.panel-heading")), 2000);
+  let suppliers = await driver.findElement(By.css("div.panel-heading")).getText(); 
+  // verify
+  await assert.equal(suppliers, "Suppliers", "Element doesn't match"); 
+  
+  await driver.quit();
+}
+
+
+catalog();
 attribute();
+manufacturers();
+suppliers();
